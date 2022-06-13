f523ea105ccf4885b4a5c46ca02a8fc0188af03c
FloKots <florencekotohoyoh@gmail.com>
2022-06-13 17:03:22 +0300

    add about us page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView
+
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
