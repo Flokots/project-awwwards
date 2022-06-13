b80bb1c8699b1561427f84adce604e73fe7e4f01
FloKots <florencekotohoyoh@gmail.com>
2022-06-13 20:17:53 +0300

    add project detail path

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProjectListView
+from .views import ProjectListView, ProjectDetailView
 from . import views
 
 urlpatterns=[
     path('', ProjectListView.as_view(), name='index'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
     path('about/', views.about, name='about'),
 ]
 
