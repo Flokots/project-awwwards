14bf222cf1a1b0d5bbdb5ea8b0dd660d901a8e8f
FloKots <florencekotohoyoh@gmail.com>
2022-06-13 13:55:03 +0300

    add bio to user profile

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200, null=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
 
     def __str__(self):
