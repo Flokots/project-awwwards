46c23f2b2806ca42254562227c739fed6a2af70c
FloKots <florencekotohoyoh@gmail.com>
2022-06-13 19:57:41 +0300

    add project list view class

--- main/views.py
+++ main/views.py
@@ -1,10 +1,18 @@
+from pyexpat import model
+from typing import List
 from django.shortcuts import render, redirect
+from django.views.generic import ListView
 from .models import Project
+
 
 def index(request):
     title='Home'
     projects= Project.objects.all()
     return render(request, 'index.html', {'title': title, 'projects': projects})
+
+
+class ProjectListView(ListView):
+    model = Project
 
 def about(request):
     title='About'
