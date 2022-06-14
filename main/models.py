970cc4dd6a6cd970d745ba478724dcebc413f37f
FloKots <florencekotohoyoh@gmail.com>
2022-06-14 10:06:30 +0300

    use image field for images:

--- main/models.py
+++ main/models.py
@@ -1,16 +1,30 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django_resized import ResizedImageField
 from PIL import Image
+from django.urls import reverse
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     link = models.CharField(max_length=100)
-    landing_page = ResizedImageField(size=[2878, 1618], crop=['middle', 'center'], default='default_land.jpg', upload_to='landscape')
+    landing_page = models.ImageField(default='default_land.jpg', upload_to='landing_pages/')
     date_posted = models.DateTimeField(default=timezone.now)
     developer = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.landing_page.path)
+
+        if img.height > 940 or img.width > 960:
+            output_size = (600, 600)
+            img.thumbnail(output_size)
+            img.save(self.landing_page.path)
+
+       
+    def get_absolute_url(self):
+        return reverse('project-detail', kwargs={'pk': self.pk})
--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=200, null=True)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+    image = models.ImageField(default='default_user.jpg', upload_to='profile_pics/')
 
     def __str__(self):
         return f'{self.user.username} Profile'
