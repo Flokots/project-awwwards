11a01efdf9e37f41166d2ae24cd4cf6d11706651
FloKots <florencekotohoyoh@gmail.com>
2022-06-14 07:29:53 +0300

    add new project path

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import ProjectListView, ProjectDetailView
+from .views import (
+    ProjectListView, 
+    ProjectDetailView,
+    ProjectCreateView,
+)
 from . import views
 
 urlpatterns=[
     path('', ProjectListView.as_view(), name='index'),
     path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('project/new/', ProjectCreateView.as_view(), name='project-create'),
     path('about/', views.about, name='about'),
 ]
 
