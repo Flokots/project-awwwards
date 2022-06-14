d4a9d96576d5df0d82c9bbbc25efd899db402b45
FloKots <florencekotohoyoh@gmail.com>
2022-06-14 07:58:53 +0300

    use resized image field for landing page

--- main/models.py
+++ main/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django_resized import ResizedImageField
+from PIL import Image
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     link = models.CharField(max_length=100)
-    landing_page = models.ImageField(default='default.jpg', upload_to='landing_pages/')
+    landing_page = ResizedImageField(size=[2878, 1618], crop=['middle', 'center'], default='default_land.jpg', upload_to='landscape')
     date_posted = models.DateTimeField(default=timezone.now)
     developer = models.ForeignKey(User, on_delete=models.CASCADE)
 
