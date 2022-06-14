bdc20d0b6e0c11498beaf8fabf8a4f9d9cd526b0
FloKots <florencekotohoyoh@gmail.com>
2022-06-14 10:59:13 +0300

    add user passes test mixin

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, 
     DetailView, 
@@ -36,13 +36,19 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class ProjectUpdateView(LoginRequiredMixin, UpdateView):
+class ProjectUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Project
     fields = ['title', 'landing_page', 'description', 'link']
 
     def form_valid(self, form):
         form.instance.developer = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        project = self.get_object()
+        if self.request.user == project.developer:
+            return True
+        return False
 
 def about(request):
     title='About'
