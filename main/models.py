385e27b51413e3456c26aa6a6bca8a391008e922
Flokots <florencekotohoyoh@gmail.com>
2024-05-28 22:00:58 +0200

    Create review model

--- main/models.py
+++ main/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from PIL import Image
 from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
@@ -14,7 +16,7 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
@@ -25,13 +27,24 @@ class Project(models.Model):
             img.thumbnail(output_size)
             img.save(self.landing_page.path)
 
-       
     def get_absolute_url(self):
         return reverse('project-detail', kwargs={'pk': self.pk})
 
-    
     @classmethod
     def search_by_title(cls, search_term):
         projects = cls.objects.filter(title__icontains=search_term)
 
         return projects
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    design_rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
+    usability_rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
+    content_rating = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10)])
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review by {self.user.username} on {self.project.title}'
